07c51e2c10cdc53c0ec5db5ca7a1ddccda58d600
OvidijusSab <Ovidijuss1@gmail.com>
2024-02-22 13:44:04 +0200

    added workTab.js styled on both screen sizes

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 import IntroModal from "./components/introTab.js"
+import WorkModal from "./components/workTab.js";
 
 document.querySelector("#introButton").addEventListener("click", function() {
   const modal = IntroModal();
+  document.body.appendChild(modal);
+  modal.showModal(); 
+});
+
+document.querySelector("#workButton").addEventListener("click", function() {
+  const modal = WorkModal();
   document.body.appendChild(modal);
   modal.showModal(); 
 });
